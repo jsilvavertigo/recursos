95df200cf43b6e9f0502e2d855352083615bba2a
jsilvavertigo <71832316+jsilvavertigo@users.noreply.github.com>
2020-11-17 13:36:07 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-@@  Docker @@
+sudo yum check-update
 curl -fsSL https://get.docker.com/ | sh
-sudo yum update -y
-sudo systemctl start docker && sudo systemctl enable docker
-@@ Nomad @@
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum update -y
+sudo systemctl start docker && sudo systemctl enable docker
 sudo yum -y install nomad
-@@ Consul @@
 sudo yum -y install consul
